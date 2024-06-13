0a995ff018d021f71e564d0bb47619ec04c81098
jaddu17 <shafajaddu@gmail.com>
2024-06-13 11:53:27 +0700

    variabel int main

--- Binary Search Tree.cpp
+++ Binary Search Tree.cpp
@@ -129,4 +129,8 @@ int main()
         cout << "4. Perform postorder traversal" << endl;
         cout << "5. Exit" << endl;
         cout << "\nEnter your choice (1-5) :" ;
+
+        char ch;
+        cin >> ch;
+        cout << endl;
 }
