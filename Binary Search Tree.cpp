bbdea364da9e1efe318d14e965bd059c2f4d1736
jaddu17 <shafajaddu@gmail.com>
2024-06-13 11:08:17 +0700

    void insert binarytree

--- Binary Search Tree.cpp
+++ Binary Search Tree.cpp
@@ -23,6 +23,23 @@ class BinaryTree
 public:
     Node* ROOT;
 
+    BinaryTree()
+    {
+        ROOT = nullptr;     // Initializing ROOT to null 
+    }
+
+    void insert(string element)     // Insert a node in the binary search tree
+    {
+        Node* newNode = new Node(element, nullptr, nullptr);        // Allocate memory for the new node
+        newNode->info = element;        // Assign value to the data field f the new node
+        newNode->leftchild = nullptr;   // Make the left child of the new node point to NULL
+        newNode->rightchild = nullptr;  // Make the right child of the new node point to NULL
+
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode);       // Locate the node which will be the parent of the node to be insert
+
+    }
 };
 
 int main()
